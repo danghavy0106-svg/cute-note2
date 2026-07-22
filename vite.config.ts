792e028d2fc201f5689caa1ef549087d15e5dcf3
danghavy0106-svg <danghavy0106@gmail.com>
2026-07-22 21:20:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  base: './',
+  base: './'
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
